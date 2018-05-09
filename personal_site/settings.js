ba540b8442c99c466dbffeb80d940b993f3e81c2
Cole Brooks <macoryx@gmail.com>
2018-05-08 20:22:28 -0400

    Add files via upload

---
+++ personal_site/settings.js
@@ -0,0 +1,24 @@
+// Update site content
+
+var config = {
+    "title": "Your Name Here",
+    "description": "Your Occupation Here"
+}
+
+var details = [
+    {
+        "title": "Programmer",
+        "icon": "code",
+        "description": "I have worked with several programming languages within my programming lifespan. I am fluent in Python, C#, C++ and PHP."
+    },
+    {
+        "title": "Aerial Photographer",
+        "icon": "camera-retro",
+        "description": "I am a beginner in Aerial Photography, I use the DJI Phantom 3 Standard to capture stunning pictures from the sky. I can also record cinematic videos."
+    },
+    {
+        "title": "Helpful Person",
+        "icon": "people-carry",
+        "description": "I enjoy helping people delve into the world of programming, I also enjoy sharing my knowledge in computers with people who need it. Often you will find me providing consultation free of charge."
+    }
+]
